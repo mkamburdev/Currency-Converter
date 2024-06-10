6bd8af5704065da022156bd401e226452343f22a
Muhammed Kambur <muhammed.kambur@dci-student.org>
2024-06-10 13:29:35 +0200

    favicon addition and mobile design adjustment

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
         inter: ["Inter", "sans-serif"],
         thasadith: ["Thasadith", "sans-serif"],
       },
+      screens: {
+        xs: "375px",
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+      },
     },
   },
   plugins: [],
-}
+};
