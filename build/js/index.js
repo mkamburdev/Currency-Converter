feff5e956ca25983799ee4d57a268adbb6235c67
Muhammed Kambur <muhammed.kambur@dci-student.org>
2024-06-24 10:33:19 +0200

    Update fetch code to use arrow function syntax and improve error handling

--- build/js/index.js
+++ build/js/index.js
@@ -6,17 +6,16 @@ submit.addEventListener("submit", function (event) {
   const to = document.querySelector("#convert-to");
 
   const url = `https://api.coinbase.com/v2/prices/${from.value}-${to.value}/spot`;
-  fetch(url).then((response) => {
-    response
-      .json()
-      .then((jsondata) => {
-        const eingabe = document.querySelector("#convert-count");
-        const output = document.querySelector("#output");
-        output.value = jsondata.data.amount * eingabe.value;
-        console.log(jsondata);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  });
+  fetch(url)
+    .then((response) => response.json())
+    .then((jsondata) => {
+      const eingabe = document.querySelector("#convert-count");
+      const output = document.querySelector("#output");
+      const convertedAmount = (jsondata.data.amount * eingabe.value).toFixed(2);
+      output.value = convertedAmount;
+      console.log(jsondata);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
